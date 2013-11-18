88ee4d838cfd277e77c78c34b68861efb77ad98a
Mike Burns <mgburns@bu.edu>
2013-11-18 19:44:05 +0000

    Formatting updates for consistency with WordPress convention

--- bu-js-lib.php
+++ bu-js-lib.php
@@ -9,29 +9,26 @@ Version: 1.0
 */
 
 // This plugin is loaded (use this for graceful degradation).
-define('BU_PLUGIN_JAVASCRIPT_LIBRARY', true);
+define( 'BU_PLUGIN_JAVASCRIPT_LIBRARY', true );
 
-class BuJavascriptLib
-{
+class BU_Javascript_Library {
 
 	// This is a blog-specific URL to the bu-js-lib root directory.
 	protected static $url;
 
-	public static function registerJS(&$scripts)
-	{
+	public static function register_js( &$scripts ) {
+
 		self::$url = sprintf('%s/mu-plugins/bu-js-lib', WP_CONTENT_URL);
 		$js = self::$url . '/js/';
-		
 
-		/* jquery plugins and addons */
-		
+		// jQuery plugins and addons
 		$scripts->add('jquery-dev', $js . 'jquery-1.3.2.dev.js', array(), '1.3.2');
 		$scripts->add('jquery-nested-sortable', $js . 'inestedsortable-1.0.1.pack.js', array('jquery'), '1.0.1');
 		$scripts->add('jquery-scrolling-tree', $js . 'jquery-scrolling-tree.js', array('jquery'), '1.0');
 		$scripts->add('jquery-dimensions', $js . 'jquery-dimensions.1.0b2.js', array('jquery'), '1.0b2');
 		$scripts->add('jquery-tooltip', $js . 'jquery-tooltip.1.3.js', array('jquery', 'jquery-dimensions'), '1.3');
 		$scripts->add('jquery-qtip', $js . 'jquery.qtip-1.0.0-rc3.min.js', array('jquery'), '1.0.0-rc3');
-		$scripts->add('jquery-qtip-dev', $js . 'jquery.qtip-1.0.0-rc3.js', array('jquery'), '1.0.0-rc3');	
+		$scripts->add('jquery-qtip-dev', $js . 'jquery.qtip-1.0.0-rc3.js', array('jquery'), '1.0.0-rc3');
 		$scripts->add('jquery-tree', $js . 'jstree/jquery.tree.min.js', array('jquery'), 0.99);
 		$scripts->add('jquery-tree-dev', $js . 'jstree/jquery.tree.js', array('jquery'), 0.99);
 		$scripts->add('jquery-tree-contextmenu', $js . 'jstree/plugins/jquery.tree.contextmenu.js', array('jquery'), 0.99);
@@ -48,22 +45,23 @@ class BuJavascriptLib
 		$scripts->add('jquery-autocomplete', $js . 'jquery.autocomplete.min.js', array('jquery'), '1.1');
 		$scripts->add('jquery-autocomplete-dev', $js . 'jquery.autocomplete.js', array('jquery'), '1.1');
 		$scripts->add('jquery-scroller', $js . 'jquery.tools.scroller.min.js', array('jquery'), '1.1.2', array('group' => 1));
-		$scripts->add('bu-modal', self::$url . '/packages/bu-modal/bu-modal.dev.js', array('jquery'), '1.4');
 
-		/* primary navigation autowidth */
-		$scripts->add('nav-autowidth', $js . 'nav-autowidth.js', array('jquery'), 1.0);
-		
 		// Mootools and friends.
 		$scripts->add('mootools', $js . 'mootools-1.2.3-core.js', array(), '1.2.3');
 		$scripts->add('mootools-dev', $js . 'mootools-1.2.3-core.dev.js', array(), '1.2.3');
+
+		// Custom BU scripts
+		$scripts->add('nav-autowidth', $js . 'nav-autowidth.js', array('jquery'), 1.0);
+		$scripts->add('bu-modal', self::$url . '/packages/bu-modal/bu-modal.dev.js', array('jquery'), '1.4');
 	}
 
-	public static function registerCSS(&$styles)
-	{
+	public static function register_css( &$styles ) {
+
 		self::$url = sprintf('%s/mu-plugins/bu-js-lib', WP_CONTENT_URL);
 		$js = self::$url . '/js/';
 		$css = self::$url . '/css/';
 
+		// jQuery plugins / jQuery UI
 		$styles->add('jquery-tree', $js . 'jstree/themes/default/style.css', FALSE, 0.99);
 		$styles->add('jquery-tree-apple', $js . 'jstree/themes/apple/style.css', FALSE, 0.99);
 		$styles->add('jquery-tree-checkbox', $js . 'jstree/themes/checkbox/style.css', FALSE, 0.99);
@@ -71,9 +69,13 @@ class BuJavascriptLib
 		$styles->add('jquery-ui', $css . 'jquery-ui-1.7.2.custom.css', FALSE, 1.72);
 		$styles->add('jquery-ui-lightness', $css . 'ui-lightness/style.css', FALSE, 1);
 		$styles->add('jquery-autocomplete', $css . 'jquery.autocomplete.css', FALSE, 1.1);
+
+		// Custom BU scripts
 		$styles->add('bu-modal', self::$url . '/packages/bu-modal/css/bu-modal.css', FALSE, '0.1');
 	}
 }
-add_action('wp_default_scripts', array('BuJavascriptLib', 'registerJS'));
-add_action('wp_default_styles', array('BuJavascriptLib', 'registerCSS'));
+
+add_action( 'wp_default_scripts', array( 'BU_Javascript_Library', 'register_js' ) );
+add_action( 'wp_default_styles', array( 'BU_Javascript_Library', 'register_css' ) );
+
 ?>
