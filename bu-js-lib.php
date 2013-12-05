3864063b3a5a3ed2e7afa06b4f3d5d4ef647ab9d
Mike Burns <mgburns@bu.edu>
2013-12-05 17:46:28 +0000

    bu-js-lib: Ensure that `is_user_logged_in()` is defined before calling it
    Prevents issues when plugins are calling WP functions too early (i.e. outside of a hook)

--- bu-js-lib.php
+++ bu-js-lib.php
@@ -39,7 +39,7 @@ class BU_Javascript_Library {
 		// Shared jQuery UI stylesheet
 		// @see http://core.trac.wordpress.org/ticket/18909
 		// @see https://github.com/helenhousandi/wp-admin-jquery-ui
-		if ( is_user_logged_in() && 'classic' == get_user_option( 'admin_color' ) ) {
+		if ( function_exists( 'is_user_logged_in' ) && is_user_logged_in() && 'classic' == get_user_option( 'admin_color' ) ) {
 			$styles->add( 'bu-jquery-ui', $css . '/jquery-ui-classic.css', array(), BU_JS_LIB_VERSION );
 		} else {
 			$styles->add( 'bu-jquery-ui', $css . '/jquery-ui-fresh.css', array(), BU_JS_LIB_VERSION );
